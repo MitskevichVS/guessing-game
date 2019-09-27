19617faff08785fe5a3f0260123e8281a28bbad7
Mitskevich Victor <mv2.mx@yandex.ru>
2019-09-27 10:28:12 +0300

    feat: add full solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,32 +1,19 @@
 class GuessingGame {
     constructor() {
-        this.array = [];
-        this.answer = 0;
         this.startIndex = 0;
         this.endIndex = 0;
         this.middleIndex = 0;
     }
 
     setRange(min, max) {
-        let minVal = min;
-        let maxVal = max; 
-        let arr = new Array;
-        while (minVal !== maxVal){
-            arr.push(minVal);
-            minVal += 1;
-        }
-        this.endIndex = arr.length - 1;
-        this.array = arr;
+        this.startIndex = min;
+        this.endIndex = max; 
     }
 
     guess() {
-        console.log(this.startIndex);
-        console.log(this.endIndex);
-        let middle = this.roundToPrecision(((this.startIndex + this.endIndex) / 2), 1);
-        this.middleIndex = middle;
-        console.log(`middle ${this.middleIndex}`);
-        this.answer = this.array[this.middleIndex];
-        return this.answer;
+        this.middleIndex = Math.ceil((this.startIndex + this.endIndex) / 2);
+
+        return this.middleIndex;
     }
 
     lower() {
@@ -35,11 +22,6 @@ class GuessingGame {
 
     greater() {
         this.startIndex = this.middleIndex;
-    }
-
-    roundToPrecision(x, precision) {
-        var y = +x + (precision === undefined ? 0.5 : precision/2);
-        return y - (y % (precision === undefined ? 1 : +precision));
     }
 }
 
